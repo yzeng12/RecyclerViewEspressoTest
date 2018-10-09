0bab8723ce50d06bc8afc01d51a43943c5adf5a8
yzeng12 <zengyx1995@gmail.com>
2018-10-09 11:33:51 -0500

    Initial commit(expresso test fail problem)

---
+++ app/src/androidTest/java/com/example/android/recyclerview/MainActivityTest.java
@@ -0,0 +1,83 @@
+package com.example.android.recyclerview;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.recyclerview),
+                        childAtPosition(
+                                withClassName(is("android.support.design.widget.CoordinatorLayout")),
+                                0)));
+        recyclerView.perform(actionOnItemAtPosition(12, click()));
+
+        ViewInteraction recyclerView2 = onView(
+                allOf(withId(R.id.recyclerview),
+                        childAtPosition(
+                                withClassName(is("android.support.design.widget.CoordinatorLayout")),
+                                0)));
+        recyclerView2.perform(actionOnItemAtPosition(15, click()));
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.word), withText("Clicked! Word 15"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.recyclerview),
+                                        9),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Clicked! Word 15")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
